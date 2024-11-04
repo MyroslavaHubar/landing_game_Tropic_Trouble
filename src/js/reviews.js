69ae94b6627b921cceeeded6c406dcd1e622cdfe
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-11-04 02:11:12 +0100

    add swiper reviews

---
+++ src/js/reviews.js
@@ -0,0 +1,21 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+new Swiper('.reviews-swiper', {
+  slidesPerView: 1,
+
+  spaceBetween: 18,
+
+  autoplay: {
+    delay: 4000,
+  },
+
+  loop: true,
+
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+});
